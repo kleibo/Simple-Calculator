a77cd9f8e71887cf0da750c25334bc3165b6bc5e
kleibo <kleibo1999@gmail.com>
2023-01-15 20:53:46 -0500

    Fixed = and C button

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import ast
+import sympy
 
 class Calculator(tk.Tk):
     def __init__(self):
@@ -26,7 +26,7 @@ class Calculator(tk.Tk):
 
         self.create_button(".", 4, 1)
         self.create_button("0", 4, 2)
-        self.create_button("C", 4, 3)
+        self.create_button("C", 4, 3, command=self.button_clear)
 
         self.create_button("+", 5, 1)
         self.create_button("-", 5, 2)
@@ -37,14 +37,17 @@ class Calculator(tk.Tk):
 
     
     def create_button(self, text, row, column, command=None):
-        button = tk.Button(self, text=text, command=lambda: self.button_press(text))
+        if text != "=" and text != "C":
+            button = tk.Button(self, text=text, command=lambda: self.button_press(text))
+        else:
+            button = tk.Button(self, text=text, command=command)
         button.grid(row=row, column=column, padx=10, pady=10)
 
 
     # Method takes argument passed from lambda function and appends it to current value of the entry widget
     def button_press(self, number):
         current = self.entry.get()
-        self.entry.delte(0, tk.END)
+        self.entry.delete(0, tk.END)
         self.entry.insert(0, str(current) + str(number))
 
     # Method clears entry when pressed
@@ -54,9 +57,12 @@ class Calculator(tk.Tk):
     def calculate(self):
         current = self.entry.get()
         try:
-            self.total.set(ast.literal_eval(current))
+            result = sympy.parse_expr(current).evalf()
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, result)
         except (ValueError, SyntaxError):
-            self.total.set(("Error"))
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, "Error")
 
 if __name__ == "__main__":
     calculator = Calculator()
